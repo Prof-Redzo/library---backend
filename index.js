0ee53548e45aa74448cbc547abf8f87fb84bfb3f
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-06-12 14:54:43 +0200

    Created MongoDB base

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+//mongoDB base
+mongoose.connect("mongodb://localhost:27017/library---backend", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// schema for books
+const Book = mongoose.model("Book", {
+  title: String,
+  content: String
+});
+
+// schema for admin user
+const User = mongoose.model("User", {
+  email: String,
+  password: String
+});
 
 app.get('/', (req, res) => {
   res.send("Working");
