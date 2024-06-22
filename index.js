4196964d7fdf27f2f4ff76fa33a956f1ca920569
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-06-22 17:10:15 +0200

    deleted duplicat code from index.js

--- index.js
+++ index.js
@@ -10,27 +10,6 @@ await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`) }
 connectToDb().then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-// schema for books
-const Book = mongoose.model("Book", {
-  title: String,
-  inventoryNumber: Number,
-  authorsName: String,
-  publicationYear: {type: Date, default: Date.now},
-  publicationPlace: String,
-  publisher: String
-});
-
-// schema for admin user
-const User = mongoose.model("User", {
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  phone: Number,
-  startDate: {type: Date, default: Date.now},
-  expiryDate: {type: Date}
-});
-
 app.get('/', (req, res) => {
   res.send("Working");
 });
