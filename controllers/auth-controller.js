cdc2860b0a5520210d0176063692ec48921486e2
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-06-24 18:52:22 +0200

    added registration

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,3 +2,16 @@ import bcrypt from "bcrypt";
 
 import User from "../models/User.js";
 
+export const registerUser = async (req,res) => {
+  const data = req.body;
+  const userExists = await User.findOne({ email: data.email });
+
+  if(userExists) {
+    return res.status(403).send("User with that email already exists");
+  } else {
+    const newUser = new User(data);
+     await newUser.save();
+     return res.status(201).send("User created successfully!");
+  }
+}
+ 
