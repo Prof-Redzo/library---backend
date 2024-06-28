c459632616dbadaadaf64287287c97ad9dcecb23
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-06-28 17:28:22 +0200

    finished user registration

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,14 +4,23 @@ import User from "../models/User.js";
 
 export const registerUser = async (req,res) => {
   const data = req.body;
-  const userExists = await User.findOne({ email: data.email });
+  try {
+    
+    const userExists = await User.findOne({ email: data.email });
+    if (userExists) {
+      return res.status(403).send("User with that email already exists");
+    }
 
-  if(userExists) {
-    return res.status(403).send("User with that email already exists");
-  } else {
+    const hashedPassword = await bcrypt.hash(data.password, parseInt(process.env.SALT_ROUNDS));
+    data.password = hashedPassword;
+
     const newUser = new User(data);
-     await newUser.save();
-     return res.status(201).send("User created successfully!");
+    const result = await newUser.save();
+
+    return res.status(201).send("User created successfully!");
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Could not create user");
   }
-}
- 
+};
