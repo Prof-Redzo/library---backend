79ccb8351d50fc97f577d462c8bd7f070cd73aeb
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-06-29 17:16:00 +0200

    created login funcionality

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/auth-controller.js";
+import { registerUser, login } from "../controllers/auth-controller.js";
 
 
 const router = express.Router();
 
 router.post("/auth/register", registerUser);
+router.post("/login", login);
+
 export default router;
