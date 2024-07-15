cc0e5ef65139c4688a22fb11371970cb22070c21
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-07-15 17:44:00 +0200

    added delete book funcionality

--- routes/book-router.js
+++ routes/book-router.js
@@ -1,9 +1,11 @@
 import express from "express";
 import verify from "../middlewares/auth-middleware.js";
 import { createBook } from "../controllers/book-controller.js";
+import { deleteBook } from "../controllers/book-controller.js";
 
 const router = express.Router(); 
 
 router.post('/books', verify, createBook);
+router.delete("/books/:id", verify, deleteBook);
 
 export default router;
