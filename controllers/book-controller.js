3a3ca94c4a178478cadfa4d84218bef43328c57b
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-07-17 11:31:17 +0200

    added book update funcionality

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -39,3 +39,31 @@ if (result) {
   res.status(500).send("Something went wrong");
 }
 };
+
+export const updateBook = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    
+    const allowedUpdates = ['title', 'authorsName'];
+    const updates = Object.keys(req.body);
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    const book = await Book.findOne({ _id: id, createdBy: req.user._id });
+
+    if (!book) {
+      return res.status(404).send({ error: 'Book not found or you do not have permission to update this book' });
+    }
+
+    updates.forEach(update => book[update] = req.body[update]);
+    await book.save();
+
+    res.status(200).send({ message: 'Book updated successfully', book });
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
--- routes/book-router.js
+++ routes/book-router.js
@@ -2,10 +2,12 @@ import express from "express";
 import verify from "../middlewares/auth-middleware.js";
 import { createBook } from "../controllers/book-controller.js";
 import { deleteBook } from "../controllers/book-controller.js";
+import { updateBook } from "../controllers/book-controller.js";
 
 const router = express.Router(); 
 
 router.post('/books', verify, createBook);
 router.delete("/books/:id", verify, deleteBook);
+router.put("/books/:id", verify, updateBook);
 
 export default router;
